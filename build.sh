db169258e46c322f33774b8c899d25e49185ce66
Martin Finkel <martin@videolabs.io>
2019-03-25 18:14:40 +0100

    Simplify and add msvc build target

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-cargo rustc --lib --target=x86_64-pc-windows-gnu -- -C linker=x86_64-w64-mingw32-gcc
+# on WSL
+cargo rustc --release --lib --target=x86_64-pc-windows-gnu -- -C linker=x86_64-w64-mingw32-gcc
+
+## on Windows
+cargo rustc --release --lib --target=x86_64-pc-windows-msvc
