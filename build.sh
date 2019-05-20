6fe0bbc244381ecd678091802549a7a6f9b48127
Martin Finkel <martin@videolabs.io>
2019-05-20 08:51:35 +0700

    add rustc command

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ cargo rustc --release --lib --target=x86_64-pc-windows-gnu -- -C linker=x86_64-w
 
 ## on Windows
 cargo rustc --release --lib --target=x86_64-pc-windows-msvc
+
+path\to\custom\rustc.exe src\lib.rs --crate-type cdylib --crate-name hello_world --target x86_64-uwp-windows-msvc
